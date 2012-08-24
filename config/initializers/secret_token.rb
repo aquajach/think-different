359b1234e8da53b496405a7a4bc51f740e701290
aquajach <aquajach@gmail.com>
2012-08-24 19:06:09 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinkDifferent::Application.config.secret_token = '9722020cb96f4cee18ba06c70b49d7627b0c6df6eb3937d5eba9599e08bc33abd1bb31ba8d8870bbb9118f4055a74913fb122950529851969262df8380e91a1f'
