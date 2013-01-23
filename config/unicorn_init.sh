675b3688fe1a1a975f68cc34572ba02d685dc73a
aquajach <aquajach@gmail.com>
2013-01-23 17:51:54 +0800

    revise unicorn_init.sh

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -14,7 +14,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/jack/apps/think-different/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; /home/jack/.rbenv/shims/bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=jack
 set -u
 
